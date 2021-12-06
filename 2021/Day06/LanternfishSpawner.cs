8f3fa51a19aed9b5b5766877f91fcd7a62c571aa
arntj <arntj@no-reply>
2021-12-06 09:31:14 +0100

    Day 6

---
+++ 2021/Day06/LanternfishSpawner.cs
@@ -0,0 +1,41 @@
+﻿namespace Day06
+{
+    public static class LanternfishSpawner
+    {
+        public static long RunForDays(string input, int days)
+        {
+            List<int> fishes = input.Split(',').Select(int.Parse).ToList();
+
+            long result = fishes.Count;
+
+            // creating an array to count how many fishes will be spawned every day
+            long[] spawns = new long[days];
+
+            // add initial fishes
+            foreach(int f in fishes)
+            {
+                for (int i = f; i < days; i += 7)
+                {
+                    spawns[i] += 1;
+                }
+            }
+
+            // working through every day and gradually add more fish as they spawn
+            for (int i = 0; i < days; i++)
+            {
+                long newFishes = spawns[i];
+                if (newFishes > 0)
+                {
+                    result += newFishes;
+                    // adding spawn rates for new fishes
+                    for (int j = i + 9; j < days; j += 7)
+                    {
+                        spawns[j] += newFishes;
+                    }
+                }
+            }
+
+            return fishes.Count + spawns.Sum();
+        }
+    }
+}
---
+++ 2021/Day06/Program.cs
@@ -0,0 +1,10 @@
+﻿using Day06;
+using System.Reflection;
+
+string dataPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+string[] data = File.ReadAllLines(Path.Combine(dataPath, @"Data\input.txt"));
+
+long partOne = LanternfishSpawner.RunForDays(data[0], 80);
+long partTwo = LanternfishSpawner.RunForDays(data[0], 256);
+Console.WriteLine($"Result part one: {partOne}");
+Console.WriteLine($"Result part two: {partTwo}");
