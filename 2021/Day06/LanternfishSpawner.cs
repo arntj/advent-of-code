b53ee930f9659ded977bdd7c34604ac561461139
arntj <arntj@no-reply>
2021-12-06 16:17:43 +0100

    Another minor improvement

--- 2021/Day06/LanternfishSpawner.cs
+++ 2021/Day06/LanternfishSpawner.cs
@@ -7,7 +7,7 @@
             IEnumerable<int> initialFishes = input.Split(',').Select(int.Parse);
 
             // create an index for how many fishes is in each state (days left to next spawn)
-            long[] fishes = Enumerable.Range(0, 9).Select(i => (long)initialFishes.Where(fish => fish == i).Count()).ToArray();
+            long[] fishes = Enumerable.Range(0, 9).Select(i => (long)initialFishes.Count(fish => fish == i)).ToArray();
 
             for (int i = 0; i < days; i++)
             {
