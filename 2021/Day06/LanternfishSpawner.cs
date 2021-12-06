83571bdb7d2ce2e7e164704ab7f343c964d04d9d
arntj <arntj@no-reply>
2021-12-06 09:33:33 +0100

    Minor corrections day 6

--- 2021/Day06/LanternfishSpawner.cs
+++ 2021/Day06/LanternfishSpawner.cs
@@ -4,15 +4,13 @@
     {
         public static long RunForDays(string input, int days)
         {
-            List<int> fishes = input.Split(',').Select(int.Parse).ToList();
-
-            long result = fishes.Count;
+            IEnumerable<int> initialFishes = input.Split(',').Select(int.Parse);
 
             // creating an array to count how many fishes will be spawned every day
             long[] spawns = new long[days];
 
             // add initial fishes
-            foreach(int f in fishes)
+            foreach(int f in initialFishes)
             {
                 for (int i = f; i < days; i += 7)
                 {
@@ -26,7 +24,6 @@
                 long newFishes = spawns[i];
                 if (newFishes > 0)
                 {
-                    result += newFishes;
                     // adding spawn rates for new fishes
                     for (int j = i + 9; j < days; j += 7)
                     {
@@ -35,7 +32,7 @@
                 }
             }
 
-            return fishes.Count + spawns.Sum();
+            return initialFishes.Count() + spawns.Sum();
         }
     }
 }
