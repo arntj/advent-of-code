523ec5a7b8e028284907f638d3a5312a220f9fed
arntj <arntj@no-reply>
2021-12-07 07:04:37 +0100

    Day 7

---
+++ 2021/Day07/CrabFuelEstimator.cs
@@ -0,0 +1,34 @@
+﻿namespace Day07
+{
+    public static class CrabFuelEstimator
+    {
+        public static int EstimatePartOne(string input)
+        {
+            Func<int, int, int> fuelEstimator = (int fromPos, int toPos) => Math.Abs(fromPos - toPos);
+
+            return EstimateLeastFuelConsumption(input, fuelEstimator);
+        }
+
+        public static int EstimatePartTwo(string input)
+        {
+            Func<int, int, int> fuelEstimator = (int fromPos, int toPos) => SumConsecutiveNumbers(Math.Abs(fromPos - toPos));
+
+            return EstimateLeastFuelConsumption(input, fuelEstimator);
+        }
+
+        private static int EstimateLeastFuelConsumption(string input, Func<int, int, int> fuelEstimator)
+        {
+            IEnumerable<int> crabs = input.Split(',').Select(int.Parse);
+
+            return Enumerable
+                .Range(0, crabs.Max() + 1)
+                .Select(pos => crabs.Select(c => fuelEstimator(c, pos)).Sum())
+                .Min();
+        }
+
+        private static int SumConsecutiveNumbers(int toN)
+        {
+            return toN * (toN + 1) / 2;
+        }
+    }
+}
---
+++ 2021/Day07/Program.cs
@@ -0,0 +1,10 @@
+﻿using Day07;
+using System.Reflection;
+
+string dataPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+string[] data = File.ReadAllLines(Path.Combine(dataPath, @"Data\input.txt"));
+
+int partOne = CrabFuelEstimator.EstimatePartOne(data[0]);
+int partTwo = CrabFuelEstimator.EstimatePartTwo(data[0]);
+Console.WriteLine($"Result part one: {partOne}");
+Console.WriteLine($"Result part two: {partTwo}");
