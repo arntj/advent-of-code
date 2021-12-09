e62c1a6ddaec93ae8ee9d52d36d2078e21830af2
arntj <arntj@no-reply>
2021-12-09 17:07:05 +0100

    Added day 9

---
+++ 2021/Day09/HeightMapSeeker.cs
@@ -0,0 +1,96 @@
+﻿namespace Day09
+{
+    public static class HeightMapSeeker
+    {
+        public static int FindLowPoints(string[] data)
+        {
+            int rows = data.Length;
+            int cols = data[0].Length;
+            int result = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    bool lowPoint =
+                        (data[i][j] < '9')
+                        && (j == 0 || data[i][j] < data[i][j - 1])
+                        && (j == cols - 1 || data[i][j] < data[i][j + 1])
+                        && (i == 0 || data[i][j] < data[i - 1][j])
+                        && (i == rows - 1 || data[i][j] < data[i + 1][j]);
+
+                    if (lowPoint)
+                    {
+                        result += (data[i][j] - '0') + 1;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static int FindBasins(string[] data)
+        {
+            int rows = data.Length;
+            int cols = data[0].Length;
+            int[,] input = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    input[i, j] = int.Parse(data[i][j] + "");
+                }
+            }
+
+            List<int> basinSizes = new List<int>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int currData = input[i, j];
+
+                    if (-1 < currData && currData < 9)
+                    {
+                        basinSizes.Add(MarkBasin(input, i, j));
+                    }
+                }
+            }
+
+            return basinSizes.OrderByDescending(x => x).Take(3).Aggregate(1, (int acc, int x) => acc * x);
+        }
+
+        private static int MarkBasin(int[,] data, int i, int j)
+        {
+            if (data[i, j] < 0 || data[i, j] == 9)
+            {
+                return 0;
+            }
+
+            int count = 0;
+
+            data[i, j] = -1;
+            count += 1;
+
+            if (i > 0)
+            {
+                count += MarkBasin(data, i - 1, j);
+            }
+            if (i < data.GetLength(0) - 1)
+            {
+                count += MarkBasin(data, i + 1, j);
+            }
+            if (j > 0)
+            {
+                count += MarkBasin(data, i, j - 1);
+            }
+            if (j < data.GetLength(1) - 1)
+            {
+                count += MarkBasin(data, i, j + 1);
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 2021/Day09/Program.cs
@@ -0,0 +1,10 @@
+﻿using Day09;
+using System.Reflection;
+
+string dataPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+string[] data = File.ReadAllLines(Path.Combine(dataPath, @"Data\input.txt"));
+
+int partOne = HeightMapSeeker.FindLowPoints(data);
+int partTwo = HeightMapSeeker.FindBasins(data);
+Console.WriteLine($"Result part one: {partOne}");
+Console.WriteLine($"Result part two: {partTwo}");
