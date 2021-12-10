7fba36595989dcddbfbb9735bdc5503d4b84edcc
arntj <arntj@no-reply>
2021-12-10 08:12:21 +0100

    Day 10

---
+++ 2021/Day10/Program.cs
@@ -0,0 +1,10 @@
+﻿using Day10;
+using System.Reflection;
+
+string dataPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+string[] data = File.ReadAllLines(Path.Combine(dataPath, @"Data\input.txt"));
+
+int partOne = SyntaxChecker.CalculateErrorScore(data);
+long partTwo = SyntaxChecker.CalculateCompletionScore(data);
+Console.WriteLine($"Result part one: {partOne}");
+Console.WriteLine($"Result part two: {partTwo}");
---
+++ 2021/Day10/SyntaxChecker.cs
@@ -0,0 +1,74 @@
+﻿namespace Day10
+{
+    public static class SyntaxChecker
+    {
+        public static int CalculateErrorScore(string[] input)
+        {
+            return input
+                .Select(CheckLine)
+                .Select(result => result.illegalCloser switch
+                {
+                    null => 0,
+                    ')' => 3,
+                    ']' => 57,
+                    '}' => 1197,
+                    '>' => 25137,
+                    _ => throw new ArgumentOutOfRangeException(result.illegalCloser.Value.ToString())
+                })
+                .Sum();
+        }
+        
+        public static long CalculateCompletionScore(string[] input)
+        {
+            var orderedScores = input
+                .Select(CheckLine)
+                .Where(result => result.illegalCloser == null)
+                .Select(result => CalculateCompletionScoreForLine(result.unclosedCharacters))
+                .OrderByDescending(x => x);
+
+            return orderedScores.ElementAt(orderedScores.Count() / 2);
+        }
+
+        private static long CalculateCompletionScoreForLine(char[] unclosedCharacters)
+        {
+            return unclosedCharacters
+                .Select(c => c switch
+                {
+                    '(' => 1,
+                    '[' => 2,
+                    '{' => 3,
+                    '<' => 4,
+                    _ => throw new ArgumentOutOfRangeException(c.ToString())
+                })
+                .Aggregate((long)0, (sum, c) => sum * 5 + c);
+        }
+
+        private static (char[] unclosedCharacters, char? illegalCloser) CheckLine(string line)
+        {
+            Stack<char> delimiters = new();
+            char[] openers = new[] { '(', '[', '{', '<' };
+            char[] closers = new[] { ')', ']', '}', '>' };
+
+            foreach (char c in line)
+            {
+                if (openers.Contains(c))
+                {
+                    delimiters.Push(c);
+                    continue;
+                }
+
+                int index = Array.IndexOf(closers, c);
+
+                if (index >= 0)
+                {
+                    if (delimiters.Pop() != openers[index])
+                    {
+                        return (delimiters.ToArray(), c);
+                    }
+                }
+            }
+
+            return (delimiters.ToArray(), null);
+        }
+    }
+}
