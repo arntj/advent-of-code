76b17e475a43eb8ae21e9b3b040e5612aa889a70
arntj <arntj@no-reply>
2021-12-14 08:07:44 +0100

    Day 14

---
+++ 2021/Day14/Polymerizator.cs
@@ -0,0 +1,72 @@
+﻿namespace Day14
+{
+    public static class Polymerizator
+    {
+        public static long InsertPolymers(string[] input, int steps)
+        {
+            string template = input[0];
+            Dictionary<string, char> insertionRules =
+                input[2..].ToDictionary(line => line[..2] + "", line => line[6]);
+
+            Dictionary<string, long> polymerPairs = new();
+
+            for (int i = 1; i < template.Length; i++)
+            {
+                string currentPair = template[(i - 1)..(i + 1)] + "";
+                IncrementOrAdd(polymerPairs, currentPair, 1L);
+            }
+
+            Dictionary<char, long> counts = new();
+
+            foreach (char c in template)
+            {
+                IncrementOrAdd(counts, c, 1L);
+            }
+
+            foreach (char c in insertionRules.Values)
+            {
+                if (!counts.ContainsKey(c))
+                {
+                    counts[c] = 0L;
+                }
+            }
+
+            for (int n = 0; n < steps; n++)
+            {
+                var currentPolymerPairs = polymerPairs.ToList();
+
+                foreach ((string pair, long count) in currentPolymerPairs)
+                {
+                    if (insertionRules.ContainsKey(pair))
+                    {
+                        polymerPairs[pair] = Math.Max(0L, polymerPairs[pair] - count);
+
+                        char newElement = insertionRules[pair];
+
+                        IncrementOrAdd(polymerPairs, "" + pair[0] + newElement, count);
+                        IncrementOrAdd(polymerPairs, "" + newElement + pair[1], count);
+
+
+                        IncrementOrAdd(counts, newElement, count);
+                    }
+                }
+            }
+
+            var countNumbers = counts.Values;
+
+            return countNumbers.Max() - countNumbers.Min();
+        }
+
+        private static void IncrementOrAdd<T>(Dictionary<T, long> dict, T key, long toAdd) where T : notnull
+        {
+            if (dict.ContainsKey(key))
+            {
+                dict[key] += toAdd;
+            }
+            else
+            {
+                dict[key] = toAdd;
+            }
+        }
+    }
+}
---
+++ 2021/Day14/Program.cs
@@ -0,0 +1,11 @@
+﻿using Day14;
+using System.Reflection;
+
+string dataPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+string[] data = File.ReadAllLines(Path.Combine(dataPath, @"Data\input.txt"));
+
+long partOne = Polymerizator.InsertPolymers(data, 10);
+long partTwo = Polymerizator.InsertPolymers(data, 40);
+
+Console.WriteLine($"Result part one: {partOne}");
+Console.WriteLine($"Result part two: {partTwo}");
