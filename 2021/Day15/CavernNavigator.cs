b2a8e6a583bc6f3f0625e7b4b8289ad018eac387
arntj <arntj@no-reply>
2021-12-15 22:26:48 +0100

    Day 15

---
+++ 2021/Day15/CavernNavigator.cs
@@ -0,0 +1,77 @@
+﻿namespace Day15
+{
+    public static class CavernNavigator
+    {
+        public static int FindShortestPath(string[] input, int caveSizeFactor)
+        {
+            int xLength = input[0].Length * caveSizeFactor;
+            int yLength = input.Length * caveSizeFactor;
+
+            int[][] riskLevels = new int[yLength][];
+
+            for (int y = 0; y < yLength; y++)
+            {
+                int[] line = new int[xLength];
+
+                for (int x = 0; x < xLength; x++)
+                {
+                    int inputX = x % input[0].Length;
+                    int inputY = y % input.Length;
+
+                    // find and convert number from char to int
+                    int num = input[inputY][inputX] - '0';
+
+                    // add repetition multiple in x and y direction
+                    num += (y / input.Length) + (x / input[0].Length);
+
+                    // 9 should wrap back to 1
+                    num = (num - 1) % 9 + 1;
+
+                    line[x] = num;
+                }
+
+                riskLevels[y] = line;
+            }
+
+            int[][] minRisks = new int[yLength][];
+
+            for (int i = 0; i < yLength; i++)
+            {
+                minRisks[i] = Enumerable.Repeat(int.MaxValue, xLength).ToArray();
+            }
+
+            minRisks[0][0] = 0;
+
+            PriorityQueue<(int x, int y), int> queue = new();
+            queue.Enqueue((0, 0), 0);
+
+            while (queue.Count > 0)
+            {
+                var curr = queue.Dequeue();
+                var risk = minRisks[curr.y][curr.x];
+
+                // iterate over the four neighbours
+                foreach ((int dx, int dy) in new[] {(-1, 0), (0, -1), (1, 0), (0, 1)})
+                {
+                    int x = curr.x + dx;
+                    int y = curr.y + dy;
+
+                    if (x < 0 || y < 0 || x >= xLength || y >= yLength)
+                    {
+                        continue;
+                    }
+
+                    int newRisk = risk + riskLevels[y][x];
+
+                    if (newRisk < minRisks[y][x])
+                    {
+                        minRisks[y][x] = newRisk;
+                        queue.Enqueue((x, y), newRisk);
+                    }
+                }
+            }
+
+            return minRisks[yLength - 1][xLength - 1];            
+        }
+    }
+}
---
+++ 2021/Day15/Program.cs
@@ -0,0 +1,11 @@
+﻿using Day15;
+using System.Reflection;
+
+string dataPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+string[] data = File.ReadAllLines(Path.Combine(dataPath, @"Data\input.txt"));
+
+int partOne = CavernNavigator.FindShortestPath(data, 1);
+int partTwo = CavernNavigator.FindShortestPath(data, 5);
+
+Console.WriteLine($"Result part one: {partOne}");
+Console.WriteLine($"Result part two: {partTwo}");
