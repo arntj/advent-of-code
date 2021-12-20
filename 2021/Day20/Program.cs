70a47017e1baaa80ad98fb3b8a573ffce89a7e8e
arntj <arntj@no-reply>
2021-12-20 07:46:05 +0100

    Day 20, 2021

---
+++ 2021/Day20/Program.cs
@@ -0,0 +1,16 @@
+﻿using Day20;
+using System.Reflection;
+
+string dataPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+string[] data = File.ReadAllLines(Path.Combine(dataPath, @"Data\input.txt"));
+
+(string image, string algorithm, char infiniteValue) = ImageEnhancer.ParseInput(data);
+
+(image, infiniteValue) = ImageEnhancer.Enhance(image, algorithm, infiniteValue);
+(image, infiniteValue) = ImageEnhancer.Enhance(image, algorithm, infiniteValue);
+
+int partOne = image.Where(c => c == '#').Count();
+int partTwo = 0;
+
+Console.WriteLine($"Result part one: {partOne}");
+Console.WriteLine($"Result part two: {partTwo}");
