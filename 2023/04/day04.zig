0a8de5922a4cd338058510f84987dfeeb88e9b18
Arnt Joakim Wrålsen <7281836+arntj@users.noreply.github.com>
2023-12-05 19:13:01 +0100

    Minor nitpicky change

--- 2023/04/day04.zig
+++ 2023/04/day04.zig
@@ -1,10 +1,8 @@
 const std = @import("std");
 const parser = @import("./parser.zig");
+const solver = @import("./solver.zig");
 const heap = std.heap;
 const io = std.io;
-const print = std.debug.print;
-
-const solver = @import("./solver.zig");
 
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
